a950f3da6c33d7d30fb8500114a80953d99512c7
Gtrab618 <88903401+Gtrab618@users.noreply.github.com>
2024-08-29 04:01:30 -0500

    conversion mp3 terminada

--- Controller/threadController/manageThread.py
+++ Controller/threadController/manageThread.py
@@ -1,8 +1,10 @@
+import os
 from typing import Optional
 
 from PySide6.QtCore import QThread, Signal
 from pytube import YouTube
 import re
+from moviepy.editor import VideoFileClip
 
 class downloadThread(QThread):
     finished = Signal(str)
@@ -16,12 +18,26 @@ class downloadThread(QThread):
 
     def run(self):
         try:
-            itag_macth = re.search(r"itag:\s*(\d+)", self.streamSelected)
-            itag = int(itag_macth.group(1))
+            if self.streamSelected!="audio-mp3":
+                itag_macth = re.search(r"itag:\s*(\d+)", self.streamSelected)
+                itag = int(itag_macth.group(1))
 
-            stream = self.yt.streams.get_by_itag(itag)
-            stream.download(output_path=self.location)
-            self.finished.emit(f"Video descargado en {self.location}")
+                stream = self.yt.streams.get_by_itag(itag)
+                stream.download(output_path=self.location)
+                self.finished.emit(f"Video descargado en {self.location}")
+            else:
+                stream = self.yt.streams.get_by_itag(18)
+                video_path = stream.download(output_path=self.location)
+                # Paso 2: Convertir el video a MP3
+                video_clip = VideoFileClip(video_path)
+                audio_path = video_path.replace('.mp4', '.mp3')
+                with VideoFileClip(video_path) as video_clip:
+                    video_clip.audio.write_audiofile(audio_path)
+
+                # Opcional: Elimina el archivo de video original
+                os.remove(video_path)
+
+
         except Exception as e:
             self.error.emit(str(e))
 
@@ -40,8 +56,9 @@ class searchThread(QThread):
             yt=YouTube(self.url)
             showDataStream = []
             if yt.streams:
+                showDataStream.append("audio-mp3")
                 for stream in yt.streams:
-                    print(f"itag: {stream.itag}, Tipo: {stream.mime_type}, Resolución: {stream.resolution}, Codec: {stream.codecs}")
+
                     showDataStream.append(
                         f"itag: {stream.itag}, Tipo: {stream.mime_type}, Resolución: {stream.resolution}, Codec: {stream.codecs}")
                 self.streams_ready.emit(showDataStream,yt)
